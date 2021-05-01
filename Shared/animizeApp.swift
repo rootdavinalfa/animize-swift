4752ddc0ce30936cff213f768d54c3529bc5645a
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2021-05-01 20:54:38 +0700

    Initial Commit

---
+++ Shared/animizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  animizeApp.swift
+//  Shared
+//
+//  Created by davin on 01/05/21.
+//
+
+import SwiftUI
+
+@main
+struct animizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
